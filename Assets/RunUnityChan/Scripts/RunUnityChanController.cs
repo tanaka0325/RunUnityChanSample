2f293f0be40c2bf997082f62d522171a35b129e7
Hiroyuki Tanaka <h.tanaka.0325@gmail.com>
2016-05-16 12:43:17 +0900

    tapでジャンプするように

---
+++ Assets/RunUnityChan/Scripts/RunUnityChanController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class RunUnityChanController : MonoBehaviour {
+    [SerializeField]
+    private UnityChanController unityChanController;
+    
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(ray, out raycastHit)) {
+                if (raycastHit.transform.gameObject.tag.Contains("UnityChan")) {
+                    this.unityChanController.OnTapped();
+                }
+            }
+        }
+    }
+}
